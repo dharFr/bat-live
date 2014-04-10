2f34edba06f369c8e3af39dc5820d73f63e327ac
Olivier Audard <audard@gmail.com>
2014-04-10 11:24:45 +0200

    Capturing webcam from browser, basic layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,42 @@
-console.log('\'Allo \'Allo!');
+
+(function()
+{
+    'use strict';
+
+    navigator.getMedia = ( navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia);
+
+
+    // var senderVideo = document.getElementById('sender-video');
+    var previewBtn = document.getElementById('preview-btn');
+    previewBtn.addEventListener('click', function()
+    {
+        navigator.getMedia (
+
+            // constraints
+            {
+                video: true,
+                audio: true
+            },
+
+            // successCallback
+            function(localMediaStream) {
+                var video = document.querySelector('#sender-video');
+                video.src = window.URL.createObjectURL(localMediaStream);
+                video.onloadedmetadata = function(e) {
+                    // Do something with the video here.
+                    console.log('previewing video', e);
+                    video.play();
+                };
+            },
+
+            // errorCallback
+            function(err) {
+                console.log('The following error occured:', err);
+            }
+
+        );
+    }, false);
+})();
